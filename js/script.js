888c5ad815896affe984677eaca85966c78c3ae6
Nate <studyingnate@gmail.com>
2022-06-28 20:57:20 -0700

    Adding variables functions and loops
    1. Page cap 9 students
    2. Add index to ShowPage
    3. Created for loop and conditional if statement

--- js/script.js
+++ js/script.js
@@ -11,32 +11,58 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
-
-
+//Variable to cap the total amount of students per page 
+const students_per_page = 9;
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 function showPage(list, page) {
-   
+
    // create two variables which will represent the index for the first and last student on the page
-
+      const start_index = (page * students_per_page) - students_per_page;
+      const end_index = (page * students_per_page);
   // select the element with a class of `student-list` and assign it to a variable
-
+      const student_list = document.querySelector('.student-list');
   // set the innerHTML property of the variable you just created to an empty string
+      student_list.innerHTML = "";
+      
 
   // loop over the length of the `list` parameter
-    // inside the loop create a conditional to display the proper students
+  for (let i = 0; i < list.length; i++) {
+   // inside the loop create a conditional to display the proper students
+      if (i >= start_index && i < end_index) {
       // inside the conditional:
         // create the elements needed to display the student information
         // insert the above elements
+      }
+  }
+    
 }
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
-function addPagination() {}
+function addPagination(list) {
+  // create a variable to calculate the number of pages needed
+
+  // select the element with a class of `link-list` and assign it to a variable
+
+  // set the innerHTML property of the variable you just created to an empty string
+
+  // loop over the number of pages needed
+    // create the elements needed to display the pagination button
+    // insert the above elements
+
+  // give the first pagination button a class of "active"
+
+  // create an event listener on the `link-list` element
+    // if the click target is a button:
+      // remove the "active" class from the previous button
+      // add the active class to the clicked button
+      // call the showPage function passing the `list` parameter and page to display as arguments
+   }
 
 
 // Call functions
